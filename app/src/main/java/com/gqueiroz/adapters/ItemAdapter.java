af52ac9299b0ef59c60667f62a241d2113394b48
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-02-06 18:16:10 -0200

    -Upgrade completo de versão

--- app/src/main/java/com/gqueiroz/adapters/ItemAdapter.java
+++ app/src/main/java/com/gqueiroz/adapters/ItemAdapter.java
@@ -1,22 +1,25 @@
 package com.gqueiroz.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.gqueiroz.database.Item;
 import com.gqueiroz.openwallet.R;
+import com.gqueiroz.openwallet.ItemAddRem;
 
 import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
     public List<Item> itemList;
 
-    public ItemAdapter(List<Item> itemList){
+    public ItemAdapter(List<Item> itemList) {
         this.itemList = itemList;
     }
 
@@ -33,7 +36,57 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     public void onBindViewHolder(final ItemViewHolder itemViewHolder, int position) {
         final Item item = itemList.get(position);
         itemViewHolder.itemNome.setText(item.getName());
-        itemViewHolder.itemValor.setText("R$ "+String.valueOf(item.getValue()));
+        itemViewHolder.itemValor.setText("R$ " + String.valueOf(item.getValue()));
+
+        itemViewHolder.itemAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ItemAddRem.class);
+                v.getContext().startActivity(i);
+            }
+        });
+
+        itemViewHolder.itemRem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ItemAddRem.class);
+                v.getContext().startActivity(i);
+            }
+        });
+
+        itemViewHolder.itemImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemViewHolder.backgroundAdd.setVisibility(View.GONE);
+                itemViewHolder.backgroundRem.setVisibility(View.GONE);
+                itemViewHolder.backgroundDel.setVisibility(View.GONE);
+            }
+        });
+
+        itemViewHolder.itemCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemViewHolder.backgroundDel.getVisibility() == View.VISIBLE || itemViewHolder.backgroundAdd.getVisibility() == View.VISIBLE) {
+                    itemViewHolder.backgroundAdd.setVisibility(View.GONE);
+                    itemViewHolder.backgroundRem.setVisibility(View.GONE);
+                    itemViewHolder.backgroundDel.setVisibility(View.GONE);
+                } else {
+                    itemViewHolder.backgroundAdd.setVisibility(View.VISIBLE);
+                    itemViewHolder.backgroundRem.setVisibility(View.VISIBLE);
+                    itemViewHolder.backgroundDel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        itemViewHolder.itemCard.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                itemViewHolder.backgroundAdd.setVisibility(View.GONE);
+                itemViewHolder.backgroundRem.setVisibility(View.GONE);
+                itemViewHolder.backgroundDel.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -45,15 +98,32 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
 
         protected TextView itemNome;
         protected TextView itemValor;
+
+        protected ImageView itemAdd;
+        protected ImageView itemRem;
+        protected ImageView itemDel;
         protected ImageView itemImagem;
-        protected CardView card;
+
+        protected LinearLayout backgroundAdd;
+        protected LinearLayout backgroundRem;
+        protected LinearLayout backgroundDel;
+        protected LinearLayout backgroundItem;
+
+        protected CardView itemCard;
 
         public ItemViewHolder(View v) {
             super(v);
             itemNome = (TextView) v.findViewById(R.id.itemNome);
             itemValor = (TextView) v.findViewById(R.id.itemValor);
             itemImagem = (ImageView) v.findViewById(R.id.itemImagem);
-            card = (CardView) v.findViewById(R.id.cardItem);
+            itemAdd = (ImageView) v.findViewById(R.id.itemAdd);
+            itemRem = (ImageView) v.findViewById(R.id.itemRem);
+            itemDel = (ImageView) v.findViewById(R.id.itemDel);
+            itemCard = (CardView) v.findViewById(R.id.itemCard);
+            backgroundAdd = (LinearLayout) v.findViewById(R.id.backgroundAdd);
+            backgroundRem = (LinearLayout) v.findViewById(R.id.backgroundRem);
+            backgroundDel = (LinearLayout) v.findViewById(R.id.backgroundDel);
+            backgroundItem = (LinearLayout) v.findViewById(R.id.backgroundItem);
         }
     }
 }
--- app/src/main/java/com/gqueiroz/database/DatabaseHandler.java
+++ app/src/main/java/com/gqueiroz/database/DatabaseHandler.java
@@ -11,15 +11,27 @@ import java.util.List;
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final String dbName = "openWallet";
-    private static final int dbVersion = 1;
+    private static final int dbVersion = 2;
 
-    private static final String tableName = "items";
+    private static final String tableItem = "itens";
 
     private static final String itemId = "itemId";
-    private static final String itemName = "itemName";
+    private static final String itemNome = "itemName";
     private static final String itemValue = "itemValue";
     private static final String itemImage = "itemImage";
 
+    private static final String tableReferencias = "referencias";
+
+    private static final String referenciasId = "referenciasId";
+    private static final String referenciasNome = "referenciasName";
+    private static final String referenciasItem = "referenciaItem";
+
+    private static final String tableHistorico = "historico";
+
+    private static final String historicoId = "historicoId";
+    private static final String historicoTipo = "historicoTipo";
+    private static final String historicoReferencia = "historicoReferencia";
+    private static final String historicoItem = "historicoItem";
 
     private final Item item1 = new Item(1, "Carteira", 99.90, "Carteira");
     private final Item item2 = new Item(2, "Banco", 99.90, "Banco");
@@ -32,24 +44,24 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sampleDB) {
-        sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName + "(itemId INTEGER PRIMARY KEY AUTOINCREMENT, itemName TEXT, itemValue REAL, itemImage TEXT);");
+        sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableItem + "(itemId INTEGER PRIMARY KEY AUTOINCREMENT, itemName TEXT, itemValue REAL, itemImage TEXT);");
 
         for (Item item : defaultItems)
-            sampleDB.execSQL("INSERT INTO " + tableName + " Values ('" + String.valueOf(item.getId()) + "', " +
+            sampleDB.execSQL("INSERT INTO " + tableItem + " Values ('" + String.valueOf(item.getId()) + "', " +
                     "'" + item.getName() + "', '" + String.valueOf(item.getValue()) + "', '" + item.getImage() + "');");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sampleDB, int oldVersion, int newVersion) {
-        sampleDB.execSQL("DROP TABLE IF EXISTS " + tableName);
+        sampleDB.execSQL("DROP TABLE IF EXISTS " + tableItem);
         onCreate(sampleDB);
     }
 
     public Item getItemByName(String name) {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor c = db.query(tableName, new String[]{itemId, itemName, itemValue, itemImage}, itemName + "=?", new String[]{name}, null, null, null, null);
+        Cursor c = db.query(tableItem, new String[]{itemId, itemNome, itemValue, itemImage}, itemNome + "=?", new String[]{name}, null, null, null, null);
 
         if (c != null)
             c.moveToFirst();
@@ -64,7 +76,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         List<Item> results = new ArrayList<>();
         SQLiteDatabase sampleDB = this.getReadableDatabase();
 
-        Cursor c = sampleDB.rawQuery("SELECT itemName, itemValue FROM " + tableName, null);
+        Cursor c = sampleDB.rawQuery("SELECT itemName, itemValue FROM " + tableItem, null);
 
         if (c != null && c.moveToFirst()) {
             do {
